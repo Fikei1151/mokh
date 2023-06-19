90716d33d8c65ae2b65151ab74034f7de4233d64
Fikree Hajiyusof <fikreehajiyusof@MacBook-Pro-khxng-Fikree.local>
2023-06-19 13:12:03 +0700

    5

--- app.py
+++ app.py
@@ -496,6 +496,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     #app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True, port=5001)
-    #app.run()
+    #app.run(debug=True, port=5001)
+    app.run()
 
