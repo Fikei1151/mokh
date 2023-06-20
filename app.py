98222199a7c5febcb112cf5c6dc2768fb72346ec
Fikree Hajiyusof <fikreehajiyusof@MacBook-Pro-khxng-Fikree.local>
2023-06-20 17:48:01 +0700

    data

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ scheduler.init_app(app)
 scheduler.start()
 
 if __name__ == '__main__':
-   ## db.create_all()
+    db.create_all()
     #app.run(host='0.0.0.0', port=5000)
     #app.run(debug=True, port=5001)
     app.run()
