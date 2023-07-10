5e6289694b4bd2df8e27d3c3b2300ad6215a23a0
Fikree Hajiyusof <fikreehajiyusof@MacBook-Pro-khxng-Fikree.local>
2023-07-10 18:48:19 +0700

    แก้ เว็บ

--- app.py
+++ app.py
@@ -550,7 +550,7 @@ def check_attendance():
 
         
 # if not any(job.id == 'attendance_check_job' for job in scheduler.get_jobs()) and RUN_APSCHEDULER:
-scheduler.add_job(id='attendance_check_job', func=check_attendance, trigger='cron', day_of_week='mon-fri', hour=16, minute=10)
+scheduler.add_job(id='attendance_check_job', func=check_attendance, trigger='cron', day_of_week='mon-fri', hour=19, minute=00)
 
 scheduler.start()
 
